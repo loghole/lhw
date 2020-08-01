1aed7eb33ae1b04829f633fd5ea7c0ff6c3973fb
Gleb Dubrovin <termiusone@gmail.com>
2020-08-01 12:03:11 +0300

    fix read deadlock

--- internal/queue.go
+++ internal/queue.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"errors"
+	"sync"
 	"sync/atomic"
 )
 
@@ -17,12 +18,14 @@ const (
 
 type Queue struct {
 	ch    chan []byte
+	cond  *sync.Cond
 	state int32
 }
 
 func NewQueue(capacity int) *Queue {
 	return &Queue{
 		ch:    make(chan []byte, capacity),
+		cond:  sync.NewCond(&sync.Mutex{}),
 		state: opened,
 	}
 }
@@ -34,6 +37,7 @@ func (s *Queue) Push(data []byte) error {
 
 	select {
 	case s.ch <- data:
+		s.cond.Signal()
 		return nil
 	default:
 		return ErrQueueIsFull
@@ -46,8 +50,17 @@ func (s *Queue) Read() []byte {
 
 func (s *Queue) Close() {
 	atomic.StoreInt32(&s.state, closed)
+	s.cond.Signal()
 }
 
 func (s *Queue) Next() bool {
+	s.cond.L.Lock()
+
+	if atomic.LoadInt32(&s.state) == opened && len(s.ch) == 0 {
+		s.cond.Wait()
+	}
+
+	s.cond.L.Unlock()
+
 	return atomic.LoadInt32(&s.state) == opened || len(s.ch) > 0
 }
